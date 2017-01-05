7519632877c69ee37dab191d8178434223b0fd72
Austin Jackson <austinjackson892@gmail.com>
2017-01-05 17:29:50 +0000

    Finish code refactor.

--- histstat.py
+++ histstat.py
@@ -8,13 +8,19 @@ import argparse
 import datetime
 import psutil
 import time
+import sys
 
 from socket import AF_INET, AF_INET6, SOCK_DGRAM, SOCK_STREAM
 
-try:
+if sys.platform.startswith('linux'):
+    PLATFORM = 'nix'
     from os import geteuid
-except ImportError:
+elif sys.platform.startswith('win'):
+    PLATFORM = 'win'
     from ctypes import *
+else:
+    print('Error: Platform unsupported.')
+    sys.exit(1)
 
 
 PROTOCOLS = {
@@ -24,34 +30,34 @@ PROTOCOLS = {
     (AF_INET6, SOCK_DGRAM):  'udp6'
 }
 FIELDS = [ "proto", "laddr", "lport", "raddr", "rport", "status", "pid",
-           "pname", "time", "date" ]
-PRETTIFY = "{:<5} {:<15.15} {:<5} {:<15.15} {:<5} {:<11} {:<5} {:<20.20} " \
-           "{:<8} {:<8}"
+           "pname", "time", "date", "user", "command" ]
+#PRETTIFY = '{:<5} {:<15.15} {:<5} {:<15.15} {:<5} {:<11} {:<5} {:<20.20} ' \
+#           '{:<8} {:<8} {:<20.20} {}'
 
 
 def histinit():
     header = ''
     root_check = False
     
-    try:
+    if PLATFORM == 'NIX':
         if geteuid() == 0:
             root_check = True
-    except NameError:
+    elif PLATFORM == 'WIN':
         if windll.shell32.IsUserAnAdmin() == 0:
             root_check = True
             
     if not root_check:
-        header += "(Not all process information could be determined, run" \
-        " at a higher privilege level to see everything.)\n"        
-    header += PRETTIFY.format(*FIELDS)
+        header += '(Not all process information could be determined, run' \
+                  ' at a higher privilege level to see everything.)\n'
+    
+    header += '\t'.join(FIELDS)
+    histlog(header)
 
-    print header
 
-
-def histout(interval):
+def histmain(interval):
     connections_A = psutil.net_connections()
     for c in connections_A:
-        process_conn(c)
+        histlog(process_conn(c))
 
     while True:
         time.sleep(interval)
@@ -59,7 +65,7 @@ def histout(interval):
         connections_B = psutil.net_connections()
         for c in connections_B:
             if c not in connections_A:
-                process_conn(c)
+                histlog(process_conn(c))
 
         connections_A = connections_B
 
@@ -77,46 +83,49 @@ def process_conn(c):
     if c.pid:
         try:
             pname, pid = psutil.Process(c.pid).name(), str(c.pid)
-        except:
+            user = psutil.Process(c.pid).username()
+            command = ' '.join(psutil.Process(c.pid).cmdline())
+        except: # if process closes too quickly
             pass
-    if c.status != "NONE":
+    if c.status != 'NONE':
         status = c.status
 
-    cfields = [proto, laddr, lport, raddr, rport, status, pid, pname,
-    time[:8], date[2:]]
+    cfields = [ proto, laddr, lport, raddr, rport, status, pid, pname,
+                time[:8], date[2:], user, command ]
     
-    line = PRETTIFY.format(*cfields)
-    print line
+    line = '\t'.join(map(str, cfields))
+    return line
+
+
+def histlog(line):
+    print(line)
+    
+    if log:
+        with open(log, 'a') as f:
+            f.write(line + '\n')
 
 
 def get_parser():
     parser = argparse.ArgumentParser(description='history for netstat')
-    parser.add_argument('-i', '--interval', help='specify update interval in seconds (default: 1 sec)', default=1, type=float)
-    # parser.add_argument('-l', '--log', help='log output to a text file', type=string)
-    # parser.add_argument('-v', '--verbose', help='verbose output')
+    parser.add_argument('-i', '--interval',
+                        help='specify update interval in seconds',
+                        default=1, type=float)
+    parser.add_argument('-l', '--log', help='log output to a text file',
+                        default=False, type=str)
     return parser
 
 
 def command_line_runner():
-    interval = 1
-    # log = False
-    # verbose = False
-    
     parser = get_parser()
     args = vars(parser.parse_args())
     
-    if args['interval']:
-        interval = args['interval']
-    
-    #if args['log']:
-    #    log = True
-    
-    #if args['verbose']:
-    #    verbose = True
+    interval = args['interval']
+    global log
+    log = args['log']
     
     histinit()
-    histout(interval)
+    histmain(interval)
     
 
 if __name__ == "__main__":
-command_line_runner()
+    command_line_runner()
